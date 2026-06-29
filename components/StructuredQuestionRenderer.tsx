d71aa7eed6dd5ab8e3b65c01886fc06e42a32c23
Simhateja17 <mg8751721@gmail.com>
2026-06-29 15:11:02 +0530

    Done with the color

--- components/StructuredQuestionRenderer.tsx
+++ components/StructuredQuestionRenderer.tsx
@@ -97,7 +97,7 @@ export default function StructuredQuestionRenderer({
                 width: 28,
                 height: 28,
                 borderRadius: '50%',
-                background: '#090E1C',
+                background: '#070E1EFA',
                 color: '#FFFFFF',
                 fontWeight: 700,
                 fontSize: 12,
