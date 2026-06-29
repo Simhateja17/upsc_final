de23d956ce5678a26dae8ea0a155487d94a96ce5
Simhateja17 <mg8751721@gmail.com>
2026-06-29 15:04:34 +0530

    CHanges made for the highlighting of the statements serial number

--- components/StructuredQuestionRenderer.tsx
+++ components/StructuredQuestionRenderer.tsx
@@ -97,7 +97,7 @@ export default function StructuredQuestionRenderer({
                 width: 28,
                 height: 28,
                 borderRadius: '50%',
-                background: '#2B7FFF',
+                background: '#090E1C',
                 color: '#FFFFFF',
                 fontWeight: 700,
                 fontSize: 12,
